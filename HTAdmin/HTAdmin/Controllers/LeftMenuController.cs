dbc92f9ef8da44f7ae3873722a8e507469474508
Tojanchazhikattu <tojans@rediffmail.com>
2016-07-14 00:34:39 +0530

    initial check in

---
+++ HTAdmin/HTAdmin/Controllers/LeftMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTAdmin.Controllers
+{
+    public class LeftMenuController : Controller
+    {
+        //
+        // GET: /LeftMenu/
+
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ HTAdmin/HTAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HTAdmin.MvcApplication" Language="C#" %>
